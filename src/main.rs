ff1b198c5a32ace815a32424c8d6738856b1082a
Victor Randall <victor.randall13@gmail.com>
2021-11-21 15:12:10 -0300

    playing with opengl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use bincode::{serialize, deserialize, Infinite};
 extern crate sdl2;
 extern crate gl;
 extern crate bincode;
 
-struct Mesh{
+struct Mesh{ // V.A.O
 	vertbuffer: Vec<f32>,
+	colorbuffer: Vec<f32>,
+	uvbuffer: Vec<f32>,
 	indexbuffer: Vec<usize>
 }
 
@@ -22,17 +25,21 @@ fn main() -> std::io::Result<()> {
         .build()
         .unwrap();
 	
-	let mut file = File::create("test.world")?;
+//	let mut file = File::create("test.world")?;
 	
-	let test = serialize(
-		&Mesh{//               1        2         3       4      5        6        7        8     9
-			vertbuffer: vec![-0.5f32, -0.5f32, 0.0f32, 0.5f32, -0.5f32, 0.0f32, 0.0f32, 0.5f32, 0.0f32],
-			indexbuffer: vec![1usize, 2usize, 3usize, 4usize, 5usize, 6usize, 7usize, 8usize, 9usize]
-		}, Infinite).unwrap();
+//	let test = serialize("rust", Infinite).unwrap();
+//		&Mesh{//               1        2         3       4      5        6        7        8     9
+//			vertbuffer: vec![-0.5f32, -0.5f32, 0.0f32, 0.5f32, -0.5f32, 0.0f32, 0.0f32, 0.5f32, 0.0f32],
+//			indexbuffer: vec![1usize, 2usize, 3usize, 4usize, 5usize, 6usize, 7usize, 8usize, 9usize]
+//		}, Infinite).unwrap();
 	
-	file.write_all(&[test])?;
+//	file.write_all(&test)?;
 	
 	let gl_context = window.gl_create_context().unwrap();
+	
+	let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+	
+	let mut sus = vec![0.01, 0.01, 0.01, 1.0];
 	
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -45,8 +52,18 @@ fn main() -> std::io::Result<()> {
                 _ => {}
             }
         }
+		
+		sus.iter_mut().for_each(|x| *x += 0.0005);
+		
+		unsafe{
+			gl::ClearColor(sus[0], sus[1], sus[2], sus[3]);
+		}
+		
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
-        // render window contents here
+        window.gl_swap_window();
     }
 }
 
