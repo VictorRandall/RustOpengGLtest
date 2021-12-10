34359a02c0e9d5d36bb71b4d9a67e052cf8d11f5
Victor Randall <victor.randall13@gmail.com>
2021-12-10 20:27:21 -0300

    open gl matrix math now

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main(){
 	
 	implement_vertex!(Vertex, position);
 	
-	let shape: Vec<Vertex; 3usize> = vec![
+	let shape = vec![
 		Vertex { position: [-0.5, -0.5] }, 
 		Vertex { position: [ 0.0,  0.5] }, 
 		Vertex { position: [ 0.5, -0.25] }
@@ -26,16 +26,35 @@ fn main(){
 	
 	let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 	
-	event_loop.run(move |ev, _, control_flow| {
-		
-		let mut target = display.draw();
-		target.clear_color(0.4,0.4,1.0,0.0);
-		target.finish().unwrap();
+	let vertex_shader_src = r#"
+		#version 140
+
+		in vec2 position;
+
+		uniform mat4 matrix;
+
+		void main() {
+		    gl_Position = matrix * vec4(position, 0.0, 1.0);
+		}
+	"#;
 	
-		let next_frame_time = std::time::Instant::now() +
-		    std::time::Duration::from_nanos(16_666_667);
-		*control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-		match ev {
+
+	let fragment_shader_src = r#"
+		#version 140
+
+		out vec4 color;
+
+		void main() {
+		    color = vec4(1.0, 0.0, 0.0, 1.0);
+		}
+	"#;
+	
+	let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+	
+	let mut t: f32 = -0.5;
+	event_loop.run(move |event, _, control_flow| {
+
+		match event {
 		    glutin::event::Event::WindowEvent { event, .. } => match event {
 		        glutin::event::WindowEvent::CloseRequested => {
 		            *control_flow = glutin::event_loop::ControlFlow::Exit;
@@ -43,7 +62,28 @@ fn main(){
 		        },
 		        _ => return,
 		    },
-		    _ => (),
+		    glutin::event::Event::NewEvents(cause) => match cause {
+		        glutin::event::StartCause::ResumeTimeReached { .. } => (),
+		        glutin::event::StartCause::Init => (),
+		        _ => return,
+		    },
+		    _ => return,
 		}
+
+		let next_frame_time = std::time::Instant::now() +
+		    std::time::Duration::from_nanos(16_666_667);
+		*control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+		// we update `t`
+		t += 0.005;
+		if t > 0.5 {
+		    t = -0.5;
+		}
+
+		let mut target = display.draw();
+		target.clear_color(0.0, 0.0, 1.0, 1.0);
+		target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t },
+		&Default::default()).unwrap();
+		target.finish().unwrap();
 	});
 }
